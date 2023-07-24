bd69021194adf5a96a205548f9a6245b05f268eb
ahsanulhaq <ulhaq.ahsan777@gmail.com>
2023-07-24 14:30:54 +0500

    removed unneccessary imports from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
-import { Button } from "@/components/ui/button"
-import {FiShoppingCart} from "react-icons/fi"
-import Image from "next/image"
-import { Input } from "@/components/ui/input"
 import Home from "@/components/Home"
-import { ShoppingCart } from "lucide-react"
 
 export default function page() {
   return (
