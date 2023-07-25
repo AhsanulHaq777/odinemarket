27712fd561f1cf4afff2cd9dbf78cbd0ce7fe180
ahsanulhaq <ulhaq.ahsan777@gmail.com>
2023-07-25 23:58:30 +0500

    implement product details page i.e. dynamic route

--- src/app/products/[title]/page.tsx
+++ src/app/products/[title]/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Link from "next/link"
 import Image from "next/image"
+import { Button } from '@/components/ui/button'
 import {client} from '@/lib/sanityClient'
 import imageUrlBuilder from '@sanity/image-url'
 import { SanityImageSource } from "@sanity/image-url/lib/types/types"
 import { groq } from 'next-sanity'
+import { ShoppingCart } from 'lucide-react'
 
 const builder = imageUrlBuilder(client)
 function urlFor(source: File | SanityImageSource) {
@@ -34,18 +36,31 @@ export default async function page({params}: { params : { title : string } }) {
   const newParam = params.title.replace(/%20/gi, " "); 
   const productData: IProduct[] = await getProductDetail(newParam);
   return (
-    <div className=" flex flex-row gap-[85px] mt-20 mb-36">
+    <>
+    <div className=" mt-28 mb-36">
           {productData.map((item)=>(
-            <Link href={`/products/${item.title}`} key={item._id}>
-             <div>
-                <Image src={urlFor(item.image).url()} width='280' height='280' alt={item.title}/>
-                  <h2 className="text-gray-700 text-xl font-extrabold font-mono mt-3">{item.title}</h2>
-                  <p className="text-gray-400 text-xl font-bold font-mono mt-1">{item.item}</p>
-                  <p className=" text-gray-700 mt-2 font-extrabold font-mono text-2xl">${item.price}</p>
-             </div>
-            </Link>
+            <div className='flex flex-row gap-10'>
+              <div><Image src={urlFor(item.image).url()} width='100' height='100' alt={item.title}/></div>
+              <div className='flex flex-row gap-10'>
+                <Image src={urlFor(item.image).url()} width='750' height='750' alt={item.title}/>
+                <div className='mt-12'>
+                  <h2 className="text-gray-700 text-3xl font-mono mt-3">{item.title}</h2>
+                  <p className="text-gray-400 text-2xl font-bold font-mono mt-1">{item.item}</p>
+                  <div className='flex flex-row mt-5 gap-5 items-center'>
+                    <button className=" bg-black tracking-tighter justify-center items-center flex px-4 py-2 text-white font-mono text-lg font-semibold gap-3">
+                      <ShoppingCart/>Add to Cart
+                    </button>
+                    <p className=" text-gray-700 mt-2 font-extrabold font-mono text-3xl">${item.price}.00</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
             ))
-}
+          }
     </div>
+    
+    </>
+    
   )
 }
