db8acb912c34635c4cb2349810d7b130bc4e7551
ahsanulhaq <ulhaq.ahsan777@gmail.com>
2023-07-30 07:54:28 +0500

    code allign in cart.tsx

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -21,12 +21,7 @@ interface IProduct {
 const showCart =async () => {
     const user_id = cookies().get("user_id")?.value;
     try {
-        const res = await fetch(`http://localhost:3000/api/cart?user_id=${user_id}`)
-        // ,{
-        //     method: "GET",
-        //     body: JSON.stringify({
-        //     })
-        // }
+        const res = await fetch(`http://localhost:3000/api/cart?user_id=${user_id}`);
         const result = await res.json();
         return result;
     } catch (error) {
@@ -41,9 +36,9 @@ const getProductDetail = async(id: string)=>{
 }
 
 // create image url from sanity image
-const builder = imageUrlBuilder(client)
+const builder = imageUrlBuilder(client);
 function urlFor(source: File | SanityImageSource) {
-  return builder.image(source)
+  return builder.image(source);
 }
 
 
@@ -54,38 +49,33 @@ export default async function page() {
      var element;
     for (let index = 0; index < cartData.data.length; index++) {
          element = cartData.data[index].product_id;
-         finalProduct.push(...await getProductDetail(element))
+         finalProduct.push(...await getProductDetail(element));
     }
   return (
     <div>
-        {cartData ? <>
-            {
-                finalProduct.map((prod)=>(
-                    <div key={prod.title}>
-                        <Image src={urlFor(prod.image).url()} width='220' height='220' alt={prod.title} className=' rounded-xl'/>
-                        <h2>{prod.item}</h2>
-                        <h2>{prod.title}</h2>
-                        <h2>{prod.category.title}</h2>
-                    </div>
-                ))
-            }
-             <h2>Your cart data.</h2>
-            {cartData.data.map( async (item)=>(
-                <>
+        {
+            cartData ? 
+            <>
                 {
-                // finalProduct.map((prod)=>(
-                //     <>
-                //     <Image src={urlFor(prod.image).url()} width='220' height='220' alt={prod.title} className=' rounded-xl'/>
-                //     <h2>{prod.item}</h2>
-                //     <h2>{prod.title}</h2>
-                //     <h2>{prod.category.title}</h2>
-                //     </>
-                // ))
-            }
-                    <div key={item.id}>{item.quantity}</div>
-                </> 
-            ))}
-        </> : <h2>Your cart is emplty.</h2>}
+                    finalProduct.map((prod)=>(
+                        <div key={prod.title}>
+                            <Image src={urlFor(prod.image).url()} width='220' height='220' alt={prod.title} className=' rounded-xl'/>
+                            <h2>{prod.item}</h2>
+                            <h2>{prod.title}</h2>
+                            <h2>{prod.category.title}</h2>
+                        </div>
+                    ))
+                }
+                <h2>Your cart data.</h2>
+                {
+                    cartData.data.map( async (item)=>(
+                    <>
+                        <div key={item.id}>{item.quantity}</div>
+                    </> 
+                    ))
+                }
+            </> : <h2>Your cart is emplty.</h2>
+        }
     </div>
   )
 }
